added tania plotting changes and fixed FID computation function

DP_LkGAN.py
@@ -241,9 +241,11 @@ class DP_LkGAN:
 
     def train_gan(self,train_images):
 
-        self.train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(self.BUFFER_SIZE).batch(self.BATCH_SIZE)
-
         self.fid_setup(train_images)
+        
+        train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+        train_images = (train_images - 127.5) / 127.5
+        self.train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(self.BUFFER_SIZE).batch(self.BATCH_SIZE)
 
         self.generator = self.make_generator_model()
         self.discriminator = self.make_discriminator_model()
@@ -264,6 +266,8 @@ class DP_LkGAN:
     def plot_fid(self):
         output_string = f"d{self.desired_digit}_a{self.alpha}_b{self.beta}_g{self.gamma}_k{self.k}_c{self.c_val}_s{self.sigma}".replace(".", "")
         final_fid_df = pd.read_csv(f'fid_outputs/{output_string}.csv')
+        plt.figure()
         plt.plot(final_fid_df['FID Scores'])
-        # TODO: Add labels to plot
+        plt.xlabel("Epochs")
+        plt.ylabel("FID Score")
         plt.show()

main.py
@@ -2,17 +2,20 @@ import DP_LkGAN as dpgan
 import Evaluation as gan_eval
 import tensorflow as tf
 
+# Sean: 3,8
+# Dan: 2,9
+# Tania: 1,7
+# Joe: 4,5
+# Alex: 0,6
 
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-train_images = (train_images - 127.5) / 127.5
 
 alpha, beta, gamma, k = 0.5, 0.5, 1, 1.6
 epochs, noise_dim, num_examples_to_generate, batch_size, max_fid_test_size = 10, 100, 128, 256, 10000
 
-desired_digit_values = [1,7]
+desired_digit_values = [3,8] 
 c_values = [10]
-sigma_values = [0.05]
+sigma_values = [0.05] # [0, 0.005, 0.01, 0.05, 0.1, 0.5]
 
 # TODO: Try running a GridSearch here...
 for desired_digit in desired_digit_values:
@@ -39,9 +42,7 @@ for desired_digit in desired_digit_values:
             gan_instance.plot_fid()
 
 
-desired_pairs = [[1,7]]
-
-for pair in desired_pairs:
+for desired_digit in desired_digit_values:
     for c_val in c_values:
         for sigma in sigma_values:
             evaluator = gan_eval.Evaluation(pair=pair,